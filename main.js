2345fd2c7180661666980c65c06cd235b5c595d6
Michael Hofer <mhofer180993@gmail.com>
2018-04-08 15:40:35 -0700

    Implements crude resolution of navigation state issues, adds second pass at gesture buffer resolution

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+//Collection of cell elements/current cell index
 var cellArray = document.getElementsByClassName("cell");
+var currentCellIndex = 0;
+const MAX_CELLS = 9;
 
 //enums to clean up code a bit
 var inputEnum = {"touchStart": 0, "touchMove": 1, "touchEnd": 2, "touchHold": 3};
 var gestureEnum = {"tap": 0, "doubleTap": 1, "swipe": 2, "press": 3};
-var inputComboEnum = {"tapCombo": "0,2", "swipeCombo":"0,1,2", "pressCombo":"0,3,2"};
+var inputComboEnum = {"tapCombo": "0,2", "swipeCombo": "0,1,2", "pressCombo": "0,3,2"};
+var gestureComboEnum = {"tapCombo": "0", "doubleTapCombo": "1", "swipeCombo": "2", "pressCombo": "3", "pressSwipeCombo": "32"};
 
 //Array buffers individual input events and gestures (gestures = predefined combo of inputs)
 var inputBuffer = [];
@@ -17,6 +21,15 @@ var inputWindow = 500;
 var inputTimeout;
 var gestureTimeout;
 
+
+//crude function to keep track of what cell we are on between refreshes
+window.onload = function () {
+    if (window.location.href.toString().split("#cell-").length > 1) {
+        currentCellIndex = window.location.href.toString().split("#cell-")[1] - 1;
+    }
+    console.log("current cell index: ", currentCellIndex);
+    navigateToCell(currentCellIndex);
+}
 
 //Add all the event listeners
 document.addEventListener("touchstart", function () {
@@ -44,7 +57,7 @@ function resetInputBuffer() {
 
 function addInputToBuffer(input) {
     inputBuffer.push(input);
-    if(inputBuffer.length >= 2) {
+    if (inputBuffer.length >= 2) {
         resolveInputBuffer();
     } else {
         resetInputBuffer();
@@ -69,7 +82,7 @@ function resolveInputBuffer() {
 
 function parseInputCombo(inputString) {
     if (inputString === inputComboEnum.tapCombo) {
-        if(gestureBuffer[gestureBuffer.length - 1] === gestureEnum.tap) {
+        if (gestureBuffer[gestureBuffer.length - 1] === gestureEnum.tap) {
             addGestureToBuffer(gestureEnum.doubleTap);
         } else {
             addGestureToBuffer(gestureEnum.tap);
@@ -92,12 +105,69 @@ function addGestureToBuffer(gesture) {
 
 function resetGestureBuffer() {
     if (gestureTimeout) {
-        clearTimeout(gestureTimeout);
+        //alternate code that extends gesture buffer time if more gestures are added to the stack
+        // clearTimeout(gestureTimeout);
+        return;
     }
     gestureTimeout = setTimeout(resolveGestureBuffer, gestureWindow);
 }
 
 function resolveGestureBuffer() {
-    console.log("GESTURES: ", gestureBuffer.toString());
+    gestureTimeout = null;
+    updateCells();
     gestureBuffer = [];
+}
+
+//cell navigation code
+function updateCells() {
+    var gestureComboKey = "";
+    gestureBuffer.forEach(function (gestureValue, index) {
+        //execute the raw input gesture
+        parseGestureCombo(gestureValue.toString());
+        gestureComboKey += gestureValue;
+
+        if(gestureComboKey.length > 1) {
+            //execute any possible combo gesture that might be valid
+            parseGestureCombo(gestureComboKey)
+        }
+    });
+}
+
+//Function that turns single gestures and gesture combos into actions
+function parseGestureCombo(gestureComboKey) {
+    switch(gestureComboKey) {
+        case gestureComboEnum.tapCombo:
+
+            break;
+        case gestureComboEnum.doubleTapCombo:
+            currentCellIndex = 0;
+            navigateToCell(currentCellIndex);
+            break;
+        case gestureComboEnum.swipeCombo:
+
+            break;
+        case gestureComboEnum.pressCombo:
+            navigateToNextCell();
+            break;
+        case gestureComboEnum.pressSwipeCombo:
+            randomizeCellColor();
+            break;
+    }
+}
+
+function navigateToNextCell() {
+    currentCellIndex++;
+    if(currentCellIndex === MAX_CELLS) currentCellIndex = 0;
+    navigateToCell(currentCellIndex);
+}
+
+function navigateToCell(index) {
+    location.href = "#";
+    location.href = "#" + cellArray[index].id;
+    //alternate way to navigate between cells
+   // document.getElementById(cellArray[index].id).scrollIntoView();
+}
+
+function randomizeCellColor() {
+    document.getElementById(cellArray[currentCellIndex].id).style.background ='#'+(Math.random()*0xFFFFFF<<0).toString(16);
 }
